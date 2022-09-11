67aa671346c9bef805a00524859724fe0f9c95a6
GIOSPO <gcesar.edi@gmail.com>
2022-09-11 02:06:22 -0300

    Projetos Mobile

---
+++ Registre/app/src/main/java/com/ds2/registre/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.ds2.registre
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.ds2.registre.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Chamado quando o usuário apertar em enviar */
+    fun sendMessage(view: View) {
+        val editTextMessage = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editTextMessage.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+}
